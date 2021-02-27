0ec3b39481b402fa3ba31b4d25d5cdc972bbb1ec
Kalin Rudnicki <kalin.rudnicki@gmail.com>
2021-02-27 15:22:08 -0600

    tweaked clui

--- src/main/scala/klib/utils/clui/package.scala
+++ src/main/scala/klib/utils/clui/package.scala
@@ -3,8 +3,6 @@ package klib.utils
 import klib.Implicits._
 import klib.fp.types._
 
-import scala.annotation.tailrec
-
 package object clui {
 
   def clui[V](
@@ -14,32 +12,24 @@ package object clui {
       handle: (Logger, State.Input[V], List[String]) => IO[State.Output[V]],
   ): Executable =
     (logger, _) => { // TODO (KR) : Do something with args as well?
-      @tailrec
       def loop(s: State.Input[V]): IO[Unit] = {
-        val body =
-          for {
-            args <- reader.read
-            res <- handle(logger, s, args)
-          } yield res
-
-        body.runSync match {
-          case Right(r) =>
-            r match {
-              case input: State.Input[V] =>
-                loop(input)
-              case State.Exit =>
-                ().pure[IO]
-              case State.Unknown =>
-                for {
-                  _ <- logger() { src =>
-                    src.error("Unknown Command")
-                  }
-                  res <- loop(s)
-                } yield res
-            }
-          case Left(a) =>
-            IO(throw a)
-        }
+        for {
+          args <- reader.read
+          res <- handle(logger, s, args)
+          _ <- res match {
+            case s2: State.Input[V] =>
+              loop(s2)
+            case State.Exit =>
+              ().pure[IO]
+            case State.Unknown =>
+              for {
+                _ <- logger() { src =>
+                  src.error("Unknown Command")
+                }
+                _ <- loop(s)
+              } yield ()
+          }
+        } yield ()
       }
 
       loop(initialState).wrap
