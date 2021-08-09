e7521b2878d2bc6b2f676d1fcf2eca418546e83b
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2021-08-09 17:35:46 -0500

    ImageUtils

--- klib-core/shared/src/test/scala/klib/Testing.scala
+++ klib-core/shared/src/test/scala/klib/Testing.scala
@@ -1,16 +1,18 @@
 package klib
 
+import java.io.File
+
 import klib.Implicits._
 import klib.fp.typeclass._
 import klib.fp.types._
+import klib.utils._
 
 object Testing extends App {
 
-  def doTraverse[F[_], T](
-      w: List[F[T]],
-  )(implicit
-      fTraverseList: Traverse[List, F],
-  ): F[List[T]] =
-    w.traverse
+  val file = new File("/home/kalin/Desktop/PhotosToUpload/Camera/20161223_154323.jpg")
+
+  ImageUtils
+    .getMetaData(file)
+    .runSyncOrExit(None)
 
 }
