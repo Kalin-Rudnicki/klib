bb41a26581d31324e2a3d6a740229fff2c2ace50
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2022-02-12 18:04:17 -0600

    Add ParamMap

--- klib-core-2/jvm/src/test/scala/klib/test/JvmMain.scala
+++ klib-core-2/jvm/src/test/scala/klib/test/JvmMain.scala
@@ -2,6 +2,7 @@ package klib.test
 
 import cats.data.NonEmptyList
 import cats.syntax.option.*
+import cats.syntax.traverse.*
 import com.google.common.jimfs.Configuration
 import com.google.common.jimfs.Jimfs
 import zio.*
@@ -11,14 +12,21 @@ import klib.utils.commandLine.parse.*
 
 object JvmMain extends ExecutableApp {
 
+  private def intToEither(int: Int): Either[String, Int] =
+    Either.cond(int != 0, int, "int == 0")
+
+  private def showIntTraverse(int: Option[Int]): URIO[Logger, Unit] =
+    Logger.println.info(int.traverse(intToEither))
+
   override val executable: Executable =
     Executable
-      .fromParser(Parser.singleValue[Int]("int").required.disallowExtras)
+      .fromParser(Parser.unit.disallowExtras)
       .withLayer(_ => ZLayer.succeed(()))
       .withExecute { _ =>
         for {
-          _ <- Logger.println.info("Hello World!")
-          _ <- ZIO.fail(NonEmptyList.one(Message.withUserMessage("dev", "user")))
+          _ <- showIntTraverse(None)
+          _ <- showIntTraverse(5.some)
+          _ <- showIntTraverse(0.some)
         } yield ()
       }
 
---
+++ klib-core-2/shared/src/main/scala/klib/web/ParamMap.scala
@@ -0,0 +1,70 @@
+package klib.web
+
+import cats.data.*
+import cats.syntax.either.*
+import cats.syntax.option.*
+import cats.syntax.traverse.*
+import io.circe.*
+
+import klib.fp.typeclass.*
+import klib.utils.*
+
+final class ParamMap(mapType: String, map: Map[String, String]) {
+
+  // =====| Add |=====
+
+  private def copy(f: Map[String, String] => Map[String, String]): ParamMap =
+    new ParamMap(mapType, f(map))
+
+  def addParam[T: EncodeToString](key: String, value: T): ParamMap =
+    copy(_.updated(key, implicitly[EncodeToString[T]].encode(value)))
+
+  def addParamO[T: EncodeToString](key: String, value: Option[T]): ParamMap =
+    value match {
+      case Some(value) => addParam[T](key, value)
+      case None        => this
+    }
+
+  def addJsonParam[T: Encoder](key: String, value: T): ParamMap =
+    copy(_.updated(key, Encoder[T].apply(value).noSpaces))
+
+  def addJsonParamO[T: Encoder](key: String, value: Option[T]): ParamMap =
+    value match {
+      case Some(value) => addJsonParam[T](key, value)
+      case None        => this
+    }
+
+  // =====| Get |=====
+
+  private def genGet[T](key: String)(f: Option[String] => Either[Message, T]): EitherNel[Message, T] =
+    f(map.get(key)).leftMap(NonEmptyList.one)
+
+  private def getRequired[T](key: String, f: String => Either[Message, T]): EitherNel[Message, T] =
+    genGet[T](key) { mString =>
+      for {
+        string <- mString.toRight(Message.same(s"Missing required $mapType '$key'"))
+        t <- f(string)
+      } yield t
+    }
+
+  private def getOption[T](key: String, f: String => Either[Message, T]): EitherNel[Message, Option[T]] =
+    genGet[Option[T]](key)(_.traverse(f))
+
+  def getParam[T: DecodeFromString](key: String): EitherNel[Message, T] =
+    getRequired(key, implicitly[DecodeFromString[T]].decode)
+
+  def getParamO[T: DecodeFromString](key: String): EitherNel[Message, Option[T]] =
+    getOption(key, implicitly[DecodeFromString[T]].decode)
+
+  def getJsonParam[T: Decoder](key: String): EitherNel[Message, T] =
+    getRequired(key, DecodeFromString.fromCirceDecoder[T].decode)
+
+  def getJsonParamO[T: Decoder](key: String): EitherNel[Message, Option[T]] =
+    getOption(key, DecodeFromString.fromCirceDecoder[T].decode)
+
+}
+object ParamMap {
+
+  def empty(mapType: String): ParamMap = new ParamMap(mapType, Map.empty)
+
+}
