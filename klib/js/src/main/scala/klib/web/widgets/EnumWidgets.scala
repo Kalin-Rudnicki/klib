ede19cc1fa095bbc02a583780bdd3a84553f2dd8
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2022-04-02 13:28:25 -0500

    Added ability to specify class-name modifiers

--- klib/js/src/main/scala/klib/web/widgets/EnumWidgets.scala
+++ klib/js/src/main/scala/klib/web/widgets/EnumWidgets.scala
@@ -12,11 +12,13 @@ import klib.web.widgets.CommonRaises.*
 object EnumWidgets {
 
   final case class RadioGroupDecorator(
+      groupClassModifiers: List[String] = Nil,
+      buttonClassModifiers: List[String] = Nil,
+      groupModifier: BeforeAfterModifier = BeforeAfterModifier(),
       buttonModifier: BeforeAfterModifier = BeforeAfterModifier(),
       buttonModifierFirst: BeforeAfterModifier = BeforeAfterModifier(),
       buttonModifierLast: BeforeAfterModifier = BeforeAfterModifier(),
       buttonModifierSelected: BeforeAfterModifier = BeforeAfterModifier(),
-      groupModifier: BeforeAfterModifier = BeforeAfterModifier(),
   )
 
   def radioGroup[E](
@@ -48,6 +50,14 @@ object EnumWidgets {
                 e.preventDefault()
                 if (isSelected) rh.setState(None)
               },
+              ClassName.be(
+                "widget-radio-group",
+                "element",
+                Option.when(isFirst)("first"),
+                Option.when(isLast)("last"),
+                Option.when(isSelected)("selected"),
+                decorator.buttonClassModifiers,
+              ),
             )(
               decorator.buttonModifier.after,
               Option.when(isFirst)(decorator.buttonModifierFirst.after),
@@ -55,6 +65,11 @@ object EnumWidgets {
               Option.when(isSelected)(decorator.buttonModifierSelected.after),
             )
           },
+          ClassName.b(
+            "widget-radio-group",
+            (if (s.isEmpty) "empty" else "non-empty").some,
+            decorator.groupClassModifiers,
+          ),
         )(
           decorator.groupModifier.after,
         )
